9ea474014266dd51f16c6c64b25d3c7f89ce432a
devoks <111417881+devoks@users.noreply.github.com>
2024-11-05 22:19:25 -0700

    Sandbox 3 AFT Account Import Request

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,29 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "aft-import-sandbox3" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "devavratoka+sandbox3@gmail.com"
+    AccountName               = "sandbox3"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "devavratoka+sandbox3@gmail.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {}
+
+  change_management_parameters = {
+    change_requested_by = "Devavrat Oka"
+    change_reason       = "Import Sandbox3 account in AFT"
+  }
+
+  custom_fields = {
+    group = "sandbox"
+  }
+
+  account_customizations_name = "sandbox"
+}
